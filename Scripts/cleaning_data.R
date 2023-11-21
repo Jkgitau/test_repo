55745f4af6cf6a85f0186c8c1915e94b13c51b53
jgitau <johngitau51@gmail.com>
2023-11-21 16:34:02 +0000

    More code added

--- Scripts/cleaning_data.R
+++ Scripts/cleaning_data.R
@@ -21,7 +21,7 @@ str(routine_data)
 
 
 ## Need to interogate each column
-unique(routine_data$year)
+unique(routine_data$test_u5)
 
 # For the year, replace 3018 and 18 with 2018 as thats the year of interest
 routine_data$year[routine_data$year == '18' | routine_data$year == '3018'] <- '2018'
@@ -35,22 +35,30 @@ New_dat <- routine_data
 
 unique(New_dat$test_u5)
 
+# Check how many rows in the new dataframe
+dim(New_dat)
+
 # Check if there are missing values
-which(is.na(New_dat1$test_u5))
+which(is.na(New_dat$test_u5))
 
 # Delete all rows with NAs
 New_dat1 <- na.omit(New_dat)
 
 # How many rows are removed? = 22
+dim(New_dat1)
+
 
 unique(New_dat1$conf_u5)
 
 # Replace o with 0
-New_dat1$conf_u5[df$address == 'Orange St'] <- 'Portola Pkwy'
-New_dat1[New_dat1 == 'O' <- '2018'
+New_dat$conf_u5[New_dat$conf_u5 == 'O'] <- 0
 
+#Remove all rows with -999 for the variable test_u5
+## Need to interogate each column
 
-
+kkk <- routine_data
+unique(kkk$test_u5)
+kkk$conf_u5[New_dat$conf_u5 == 'O'] <- 0
 
 str(New_dat1)
 #example of tidyverse 
